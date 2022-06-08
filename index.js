9047bc99e81c28668e7ece730ff2410b9c39fdf4
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-08 03:38:38 -0400

    commit tree ca229791c15a76a11a6366f2829e4fe7317cd002

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+var bestCustomer;
+const leastFavouriteCusomer = "somebody";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(customer) {
+    bestCustomer = customer
+}
+
+function changeLeastFavoriteCustomer() {
+
+    leastFavouriteCusomer = "somebodyelse";
+}
